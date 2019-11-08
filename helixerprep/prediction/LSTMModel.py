23c50e0e84c541c470afed418c929cf85b0c4999
Felix Stiehler <felix.stiehler87@gmail.com>
2019-11-08 18:12:58 +0100

    minor

--- helixerprep/prediction/LSTMModel.py
+++ helixerprep/prediction/LSTMModel.py
@@ -73,6 +73,7 @@ class LSTMModel(HelixerModel):
         self.parser.add_argument('-dr', '--dropout', type=float, default=0.0)
         self.parser.add_argument('-ln', '--layer-normalization', action='store_true')
         self.parser.add_argument('-att', '--attention', action='store_true')
+        self.parser.add_argument('-attw', '--attention-width', action='store_true')
 
         self.parse_args()
 
